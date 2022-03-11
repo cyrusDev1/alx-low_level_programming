8725fe8929f52bd48ad113389069c5bdfe22ca24
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-11 06:49:41 -0800

    Nomber

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: return 0
+ *
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar(10);
+	return (0);
+}
