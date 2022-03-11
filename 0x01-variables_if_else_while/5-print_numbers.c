45f61f20fee39a7c715ad47c411266c2f0053ac7
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-11 06:37:26 -0800

    Chiffres

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Return 0
+ *
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+		printf("%d", ch);
+	printf("\n");
+	return (0);
+}
