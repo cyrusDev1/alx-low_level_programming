a3984df528d64d418e0ed480e977c4ab95d60c5e
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-15 16:56:00 -0700

    Betty style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * jack_bauer prints every minute of the day of Jack Bauer
+ * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
- *
  * Return: no return
- *
  */
 
 void jack_bauer(void)
 {
 	int hr, min;
+
 	for (hr = 0; hr <= 23; hr++)
 	{
 		for (min = 0; min <= 59; min++)
