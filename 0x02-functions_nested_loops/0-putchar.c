e7e85918b0e96fba975bfd3b82f28566540ae7d1
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-15 16:29:27 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,6 +12,7 @@ int main(void)
 {
 	char chaine[10] = "_putchar";
 	int i;
+
 	for (i = 0; i<= 7; i++)
 	{
 		_putchar(chaine[i]);
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+
+}
