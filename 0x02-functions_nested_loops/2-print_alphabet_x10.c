7b36761f392ce19d9f0822ee4c393107ca03194c
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-15 16:36:30 -0700

    Betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,18 +3,20 @@
 #include "main.h"
 
 /**
- * main - prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  *
- * Reuturn: no return
+ * Return: no return
  *
  */
 
 void print_alphabet_x10(void)
 {
 	int n;
+
 	for (n = 0; n <= 9; n++)
 	{
 		int i;
+
 		for (i = 97; i <= 122; i++)
 		{
 			_putchar(i);
