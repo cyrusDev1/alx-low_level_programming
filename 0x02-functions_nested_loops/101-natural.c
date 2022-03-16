a0e5583002e0f56dc70f93f3c2bdd7df3e0eb052
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-15 17:20:13 -0700

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,12 +3,23 @@
 #include "main.h"
 
 /**
- * main - prints the first 50 Fibonacci numbers
+ * main - list all the natural numbers below 10 
+ * that are multiples of 3 or 5
  *
  * Return: return 0
  */
 
 int main(void)
 {
+	int i, sum;
 
+	for (i = 0; i <= 1024; i++)
+	{
+		if (i % 3 = 0 || i % 5 = 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
