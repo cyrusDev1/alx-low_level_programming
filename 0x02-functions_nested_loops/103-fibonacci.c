2cf0c7b9be51934526da6202e112a9118ba162ee
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-15 18:55:28 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,7 +12,8 @@ int main(void)
 	unsigned long nbr1 = 0, nbr2 = 1, follow, sum;
 
 	while (follow < 4000000)
-	{	follow = nbr1 + nbr2;
+	{	
+		follow = nbr1 + nbr2;
 		nbr1 = nbr2;
 		nbr2 = follow;
 		if ((nbr1 % 2) == 0)
