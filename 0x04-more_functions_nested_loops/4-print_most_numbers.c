2e2ceb9435adda4898371e569577dd3c27976c88
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-17 02:52:11 -0700

    Digit without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print the digit without 4 and 2
+ * @n: contain digit
+ * Return: no return
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 50 && n != 52)
+			_putchar(n);
+	}
+	_putchar(10);
+}
