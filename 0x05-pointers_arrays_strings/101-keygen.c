0db4f272b05297238c0034311715efeb188211e9
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-21 17:05:53 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 when everithing's alright
+ *
+ */
+
+int main(void)
+{
+	int n;
+	char c;
+
+	srand(time(0));
+	while (n <= 2645)
+	{
+		c = rand() % 128;
+		n += c;
+		putchar(c);
+	}
+	return (0);
+}
