5ffd620040fe0f2e494483e12b03179a5a6a2b6a
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-23 13:56:34 -0700

    Capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,26 +8,21 @@
 
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i, j;
+	char separate = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
 	while (str[i])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		for (j = 0; j < 13; j++)
 		{
-			if (str[i-1] == ' ' || 
-			str[i - 1] == '\t' ||
-			str[i - 1] == '\n' ||
-			str[i - 1] == ',' ||
-			str[i - 1] == ';' ||
-			str[i - 1] == '.' ||
-			str[i - 1] == '!' ||
-			str[i - 1] == '?' ||
-			str[i - 1] == '"' ||
-			str[i - 1] == '(' ||
-			str[i - 1] == ')' ||
-			str[i - 1] == '{' ||
-			str[i - 1] == '}')
-				str[i] -= 32;
+			if (str[i] == separate[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
 		}
 		i++;
 	}
