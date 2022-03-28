e75f47a70977bc945c4187378e225e1091e81ca5
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-28 09:48:39 -0700

    Matrice

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: first param
+ * @to: second param
+ * Return: no return
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
