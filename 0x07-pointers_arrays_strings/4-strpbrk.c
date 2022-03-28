4d0759cd4c511b31ce9129c6c1abca241d12f58a
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-28 05:19:15 -0700

    Strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: first param
+ * @accept: second param
+ * Return: returns a pointer to the byte in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		if (_strchr(accept, *s) != NULL)
+			return s;
+		s++;
+	}
+	return (NULL);
+}
+
+/**
+ * _strchr - locates a character in a string
+ * @s: first parameter
+ * @c: second parameter
+ * Return: return c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
