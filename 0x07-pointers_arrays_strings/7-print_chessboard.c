c287fed417c9b579cd70cd353af874db6a244ac8
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-03-28 11:52:36 -0700

    Chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -15,10 +15,10 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < len; i++)
 	{
-		for (j = 0; j < 8; j++)
+		for (j = 0;j< 8; j++)
 		{
 			printf("%c", a[i][j]);
 		}
-		putchar("\n");
+		printf("\n");
 	}
 }
