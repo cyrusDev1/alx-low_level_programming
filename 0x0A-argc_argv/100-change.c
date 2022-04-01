9a702380cacaa36458742ca9a6f85447cdeaa463
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-04-01 11:12:01 -0700

    Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,17 +10,14 @@
 
 int main(int argc, char *argv[])
 {
-	int cents = 0;
-	int sum = 0;
+	int cents, sum = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	cents = atoi(argv[1]);
-
 	while (cents > 0)
 	{
 		sum++;
