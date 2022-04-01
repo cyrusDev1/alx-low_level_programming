657e1895fb77c8d379807611c49e4454bab8b500
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-04-01 10:32:34 -0700

    Change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the number
+ * @argc: number of arg
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents = 0;
+	int sum = 0, nb1, nb2, nb10, nb25, nb5;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}else if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+	}else
+	{
+		cents = atoi(argv[1]);
+		nb25 = cents / 25;
+		nb10 = (cents % 25) / 10;
+		nb5 = (cents % 10) / 5;
+		nb2 = (cents % 5) / 2;
+		nb1 = (cents % 5) % 2;
+		sum = nb25 + nb10 + nb5 + nb2 + nb1;
+		printf("%d\n", sum);
+	}
+	return (0);
+}
