29dbf3503dd39f7bea9192276a64781f89509e32
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-04-04 10:36:40 -0700

    Strconcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 		len1++;
 	while (s2[len2])
 		len2++;
-	concat = malloc(sizeof(char) * (len1 + len2));
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (concat == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
