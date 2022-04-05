9e24e55ccd17edd3f9b45a68675f159a46754b89
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-04-05 03:27:04 -0700

    Args to str

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -26,7 +26,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++,k++)
+		for (j = 0; av[i][j] != '\0'; j++, k++)
 		{
 			string[k] = av[i][j];
 		}
