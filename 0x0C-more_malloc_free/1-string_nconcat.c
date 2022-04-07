212ffd20391594a58972c081fa99619054bd2c30
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-04-07 04:52:41 -0700

    String concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		*(concat + i) = *(s1 + i);
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; j <= n; j++, i++)
 		*(concat + i) = *(s2 + j);
 	concat[i] = '\0';
 	return (concat);
