5968b43f8b85984da9cba4e63ee7de6ecffe06ed
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-04-11 13:12:32 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,12 @@
 
 /**
  * main - print the file name
+ * @argc: number
+ * @argv: array
  * Return: 0
  */
 
-int main()
+int main(int argc, char *argv[])
 {
 	printf("%s\n", __FILE__);
 	return (0);
