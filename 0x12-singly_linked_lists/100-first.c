c7ca1e034aa46ec94905f9cf24c61013790f539f
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-04-21 08:54:38 -0700

    First

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * start - prints a string
+ *
+ * Return: Always 0.
+ */
+
+void start(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
