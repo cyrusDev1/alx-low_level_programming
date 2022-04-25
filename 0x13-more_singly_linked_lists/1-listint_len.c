81aa4f86eea0ab90aa431a6ab09cefd800e45e26
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-04-25 05:39:39 -0700

    Print len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_n - number of elements
+ * @h: list to count
+ * Return: return number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *other = h;
+
+	while (other)
+	{
+		count++;
+		other = other->next;
+	}
+	return (count);
+}
