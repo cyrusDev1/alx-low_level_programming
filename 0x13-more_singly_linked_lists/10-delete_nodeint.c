8745f65c4c02950d3726bb857e195c71f925a1ad
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-04-26 07:17:27 -0700

    Free at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (i < index - 1)
 	{
-		if(tmp == NULL || !(tmp->next))
+		if (tmp == NULL || !(tmp->next))
 			return (-1);
 		tmp = tmp->next;
 		i++;
