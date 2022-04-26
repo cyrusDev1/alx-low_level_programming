4f600d420411a03bff47d7b17e47146131390a34
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-04-26 06:05:13 -0700

    Insert at position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,12 +16,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (new == NULL || head == NULL)
 		return (NULL);
 	new->n = n;
 	if (idx == 0)
 	{
 		new->next = *head;
+		*head = new;
 		return (new);
 	}
 	while (cur != NULL && i < idx)
