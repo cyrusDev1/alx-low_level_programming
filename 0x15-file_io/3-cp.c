c64239fdea2d2e04a889250cea0ab54d8cc1778d
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-05-02 11:40:31 -0700

    Fix number of lines in function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,32 +17,25 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_read = open(argv[1], O_RDONLY);
 	if (fd_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_write = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	do {
 		res_read = read(fd_read, buffer, BUFSIZ);
 		if (res_read == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
+			_error(98, argv[1]);
 		}
-
 		res_write = write(fd_write, buffer, res_read);
 		if (res_write != res_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			_error(99, argv[2]);
 		}
-	}	while (res_write == BUFSIZ);
-
+	} while (res_write == BUFSIZ);
 	if (close(fd_read) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
@@ -54,6 +47,25 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
 		exit(100);
 	}
-
 	return (0);
+}
+
+/**
+ * _error - displays error
+ * @code: exit code
+ * @filename: filename
+ */
+
+void _error(int code, char *filename)
+{
+	if (code == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	else if (code == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
 }
