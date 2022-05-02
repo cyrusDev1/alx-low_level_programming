770abdceb8133436d4e620d6db0f73ad150e3447
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-05-02 05:54:00 -0700

    Open a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,6 +4,7 @@
  * create_file - creates a file
  * @filename: name of the file
  * @text_content: content of file
+ * Return: -1 or 1
  */
 
 int create_file(const char *filename, char *text_content)
@@ -16,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	
+
 	if (text_content != NULL)
 	{
 		while (text_content[i])
