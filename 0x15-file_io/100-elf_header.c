32a011035c17e06051473b2a1bcadb22390a9965
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-05-02 19:21:12 -0700

    change proto file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,22 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
+
+void check_elf(unsigned char *e_ident);
+void close_elf(int elf);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abiversion(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 
 /**
  * main - displays the information contained in the ELF header at
