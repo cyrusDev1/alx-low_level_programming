2a91ffd0a961f18f596a47a900c0383ff7d0e94a
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-14 02:15:12 +0100

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/libgm.so https://github.com/cyrusDev1/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgm.so
+wget -q --output-document=$HOME/libgm.so https://github.com/cyrusDev1/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgm.so
 export LD_PRELOAD=$HOME/libgm.so
