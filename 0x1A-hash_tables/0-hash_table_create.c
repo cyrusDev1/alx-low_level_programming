7a085e2e7142a1e652c67ae66143a6bd43428f81
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-07-01 17:20:47 +0100

    betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,20 @@
 
 hash_table_t *hash_table_create(unsigned int size)
 {
-    hash_table_t *ht = NULL;
-    unsigned int i = 0;
+	hash_table_t *ht = NULL;
+	unsigned int i = 0;
 
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL || size <= 0)
-    {
-        free(ht);
-        return (NULL);
-    }
-    ht->size = size;
-    ht->array = malloc(sizeof(hash_table_t *) * ht->size);
-    if (ht->array == NULL)
-        return (NULL);
-    for (i = 0; i < ht->size; i++)
-        ht->array[i] = NULL;
-    return (ht);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL || size <= 0)
+	{
+		free(ht);
+		return (NULL);
+	}
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_table_t *) * ht->size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < ht->size; i++)
+		ht->array[i] = NULL;
+	return (ht);
 }
