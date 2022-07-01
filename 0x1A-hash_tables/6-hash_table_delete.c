6bc310169568bc573a22fc94acc7ed584a758455
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-07-02 00:50:14 +0100

    del hash

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,26 +8,26 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i = 0;
-    hash_node_t *node;
-    hash_table_t *head = ht;
-    hash_node_t *temp;
+	unsigned long int i = 0;
+	hash_node_t *node;
+	hash_table_t *head = ht;
+	hash_node_t *temp;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        if (ht->array[i] != NULL)
-        {
-            node = ht->array[i];
-            while (node != NULL)
-            {
-                temp = node->next;
-                free(node->key);
-                free(node->value);
-                free(node);
-                node = temp;
-            }
-        }
-    }
-    free(head->array);
-    free(head);
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
 }
