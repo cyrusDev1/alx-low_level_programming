ca9f66d5ac2b23630d628250c598f61ad7722430
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-07-02 01:03:24 +0100

    sorted table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -44,6 +44,7 @@ void hash_table_delete(hash_table_t *ht);
 
 /**
  * struct shash_node_s - Node of a sorted hash table
+ *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
@@ -53,15 +54,16 @@ void hash_table_delete(hash_table_t *ht);
  */
 typedef struct shash_node_s
 {
-	char *key;
-	char *value;
-	struct shash_node_s *next;
-	struct shash_node_s *sprev;
-	struct shash_node_s *snext;
+	 char *key;
+	 char *value;
+	 struct shash_node_s *next;
+	 struct shash_node_s *sprev;
+	 struct shash_node_s *snext;
 } shash_node_t;
 
 /**
  * struct shash_table_s - Sorted hash table data structure
+ *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
@@ -71,10 +73,10 @@ typedef struct shash_node_s
  */
 typedef struct shash_table_s
 {
-	unsigned long int size;
-	shash_node_t **array;
-	shash_node_t *shead;
-	shash_node_t *stail;
+	 unsigned long int size;
+	 shash_node_t **array;
+	 shash_node_t *shead;
+	 shash_node_t *stail;
 } shash_table_t;
 
 shash_table_t *shash_table_create(unsigned long int size);
@@ -84,4 +86,11 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-#endif /* HASH_TABLES_H */
+int replace_value_s(shash_node_t **ht, const char *key, const char *value);
+int check_key_s(shash_node_t *ht, const char *key);
+shash_node_t *add_node_s(shash_node_t **head,
+	const char *key, const char *value);
+void insert_sort(shash_node_t *node, shash_table_t *ht);
+void free_list_s(shash_node_t *head);
+
+#endif
